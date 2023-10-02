38a013e1719ae071b820b2be318703f6dba4003f
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-10-02 15:11:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    let message = `This Saturday, I want to ${activity}!`
+    return message
+};
+
+function mondayWork(activity = 'go to the office') {
+    let message = `This Monday, I will ${activity}.`
+    return message;
+};
+
+function wrapAdjective(thing = 'special') {
+    function inner(addon = '*') {
+        const message = `You are ${thing}${addon}${thing}!`
+        return message
+    }
+    return inner;
+}
